5637f1b584ec70c93757af9d33f1a47676b3a401
Moe <mohamedmidani2001@gmail.com>
2024-01-21 16:12:04 -0500

    Initial Commit

---
+++ Earn it practice/Earn_it_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Earn_it_practiceApp.swift
+//  Earn it practice
+//
+//  Created by Mohamed Midani on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Earn_it_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Earn it practiceUITests/Earn_it_practiceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Earn_it_practiceUITestsLaunchTests.swift
+//  Earn it practiceUITests
+//
+//  Created by Mohamed Midani on 1/21/24.
+//
+
+import XCTest
+
+final class Earn_it_practiceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
